7d9feea2b311d4da5cf1e37bbe751680752ee4e3
Benjamin Choi <choi.bc97@gmail.com>
2022-06-02 18:36:05 -0400

    Set up basic config for app module, set up Nx cloud, adjust tsconfig options

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import {
+  appEnvironmentVariablesSchema,
+  default as appConfig,
+} from '../environments/environment';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      expandVariables: true,
+      validationSchema: appEnvironmentVariablesSchema,
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,3 +1,9 @@
+import Joi from 'joi';
+
 export const environment = {
   production: false,
 };
+
+export const appEnvironmentVariablesSchema = Joi.object({});
+
+export default () => ({});
