c99259c77080396e75bd15257df6e242768a6d7f
Benjamin Choi <choi.bc97@gmail.com>
2022-06-08 17:21:59 -0400

    Fix typo in MagicLoginStrategy's name

--- libs/api/auth/util/src/lib/guard/magic-login-auth.guard.ts
+++ libs/api/auth/util/src/lib/guard/magic-login-auth.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class MagicLoginAuthGuard extends AuthGuard('magic-login') {}
+export class MagicLoginAuthGuard extends AuthGuard('magiclogin') {}
