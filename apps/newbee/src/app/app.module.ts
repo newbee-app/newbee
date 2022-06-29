0845463ef459fc011a6189c91c1acadcc5d98f44
Benjamin Choi <choi.bc97@gmail.com>
2022-06-29 16:36:09 -0400

    Add ReactiveFormsModule globally

--- apps/newbee/src/app/app.module.ts
+++ apps/newbee/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
