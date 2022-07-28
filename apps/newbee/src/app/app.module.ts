b7cbd1cc26e39d1923c8161e9a7443a552e36108
Benjamin Choi <choi.bc97@gmail.com>
2022-07-28 17:04:29 -0400

    Enable SSR with Angular Universal

--- apps/newbee/src/app/app.module.ts
+++ apps/newbee/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientXsrfModule,
