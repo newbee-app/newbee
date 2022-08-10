3b9945bcb866a01e13f7a32808a2b71a66cd1021
Benjamin Choi <choi.bc97@gmail.com>
2022-08-10 11:02:28 -0400

    Add syntax highlighting for scss in Storybook Docs

--- apps/newbee/.storybook/preview.js
+++ apps/newbee/.storybook/preview.js
@@ -1,3 +1,8 @@
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import scss from 'react-syntax-highlighter/dist/esm/langugaes/prism/scss';
+
+SyntaxHighlighter.registerLanguage('scss', scss);
+
 export const parameters = {
   options: {
     storySort: {
