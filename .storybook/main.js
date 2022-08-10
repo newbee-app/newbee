481371a5336455b865518fd2ad13131d24ced9f1
Benjamin Choi <choi.bc97@gmail.com>
2022-08-10 11:00:28 -0400

    Update Storybook configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    // Includes Docs, Controls, Actions, Viewport, Backgrounds, Toolbars & globals, Measure & outline
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
