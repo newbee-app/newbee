895eb7ebc53a4bdbb982992933cbe6937e275274
Benjamin Choi <choi.bc97@gmail.com>
2022-09-07 21:02:03 -0400

    Create root router

---
+++ apps/newbee/src/app/routing/index.ts
@@ -0,0 +1,41 @@
+import { Injectable, NgModule } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import {
+  PreloadAllModules,
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+  TitleStrategy,
+} from '@angular/router';
+
+@Injectable()
+export class AppTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot): void {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`${title} | NewBee`);
+    }
+  }
+}
+
+// TODO set up a 404 not found page with a wildcard route
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('@newbee/newbee/auth/feature').then((m) => m.AuthModule),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  providers: [{ provide: TitleStrategy, useClass: AppTitleStrategy }],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
