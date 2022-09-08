6f5cdb9eeb12e20da0ed9471a8cdd995cf81084d
Benjamin Choi <choi.bc97@gmail.com>
2022-09-08 15:31:23 -0400

    Make utilities for testing

--- libs/newbee/auth/util/src/index.ts
+++ libs/newbee/auth/util/src/index.ts
@@ -1,1 +1,2 @@
+export * from './lib/example';
 export * from './lib/interface';
---
+++ libs/newbee/auth/util/src/lib/example/index.ts
@@ -0,0 +1,6 @@
+import { testUser1 } from '@newbee/shared/util';
+import { MagicLinkLoginLoginForm } from '../interface';
+
+export const testMagicLinkLoginLoginForm1: MagicLinkLoginLoginForm = {
+  email: testUser1.email,
+};
