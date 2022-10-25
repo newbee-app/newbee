b011d8216a91dbb71da3267bd4b1a138220ceb6c
Benjamin Choi <choi.bc97@gmail.com>
2022-10-25 19:13:50 -0400

    Consolidate reused Tailwind styles

--- libs/tailwind-preset/tailwind.config.js
+++ libs/tailwind-preset/tailwind.config.js
@@ -1,5 +1,45 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        bg: {
+          primary: {
+            light: colors.white,
+            dark: colors.neutral['700'],
+          },
+          secondary: {
+            light: colors.neutral['100'],
+            dark: colors.neutral['600'],
+          },
+        },
+        outline: {
+          primary: {
+            light: colors.gray['500'],
+            dark: colors.gray['400'],
+          },
+          secondary: {
+            light: colors.gray['200'],
+            dark: colors.gray['600'],
+          },
+        },
+        highlight: {
+          primary: {
+            light: colors.blue['500'],
+            dark: colors.blue['300'],
+          },
+        },
+        error: {
+          primary: colors.red['500'],
+        },
+        primary: {
+          light: colors.yellow['300'],
+          dark: colors.yellow['500'],
+        },
+      },
+    },
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
