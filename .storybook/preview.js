17979cca9811f911e6f1beb603d095bd21eab4ad
Benjamin Choi <choi.bc97@gmail.com>
2022-10-25 19:11:42 -0400

    Update storybook-dark-mode to work better for Tailwind

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,13 @@
+import { themes } from '@storybook/theming';
+
 export const parameters = {
   docs: {
     inlineStories: true,
+  },
+  darkMode: {
+    dark: { ...themes.dark, appBg: '#262626', appContentBg: '#404040' },
+    light: { ...themes.light, appBg: '#f5f5f5', appContentBg: 'white' },
+    stylePreview: true,
   },
   options: {
     storySort: {
