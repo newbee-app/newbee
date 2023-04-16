14978a8cda7398a37dca9ab8d0b80e2db7ad08a6
Benjamin Choi <choi.bc97@gmail.com>
2023-04-16 11:41:02 -0400

    chore: [nx migration] update-configs-jest-29

--- libs/publishable/solr-cli/jest.config.ts
+++ libs/publishable/solr-cli/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'publishable-solr-cli',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/libs/publishable/solr-cli',
