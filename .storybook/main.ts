534e52e3ba4456e7be21218047d4fbe11ebd1a86
Benjamin Choi <choi.bc97@gmail.com>
2023-04-27 19:04:31 -0400

    Remove unused addons from storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: [],
-  addons: [
-    '@storybook/addon-essentials',
-    '@sotrybook/addon-interactions',
-    '@storybook/addon-a11y',
-    'storybook-dark-mode',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/angular',
     options: {},
