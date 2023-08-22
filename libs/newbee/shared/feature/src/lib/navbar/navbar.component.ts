a21761ccf9c1340e7dfce38e7fb229f0d89be088
Ben Choi <choi.bc97@gmail.com>
2023-08-22 16:04:35 -0400

    Implement include center logic for the navbar

--- libs/newbee/shared/feature/src/lib/navbar/navbar.component.ts
+++ libs/newbee/shared/feature/src/lib/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, Scroll } from '@angular/router';
 import {
   AuthActions,
   authFeature,
@@ -13,6 +13,7 @@ import {
 import { ShortUrl } from '@newbee/newbee/shared/util';
 import type { Organization } from '@newbee/shared/util';
 import { Store } from '@ngrx/store';
+import { combineLatest, Subject, takeUntil } from 'rxjs';
 
 /**
  * The smart UI for the navbar.
@@ -28,7 +29,12 @@ import { Store } from '@ngrx/store';
   ],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  /**
+   * Emit to unsubscribe from all infinite observables.
+   */
+  private readonly unsubscribe$ = new Subject<void>();
+
   /**
    * The logged in user.
    */
@@ -39,7 +45,51 @@ export class NavbarComponent {
    */
   orgState$ = this.store.select(organizationFeature.selectOrgState);
 
+  /**
+   * Whether to include the center piece of the authenticated navbar.
+   */
+  includeCenter = false;
+
   constructor(private readonly store: Store, private readonly router: Router) {}
+
+  /**
+   * Look at the current URL, selected organization, and adjust `includeCenter` to `true` if it's past the org's home page.
+   */
+  ngOnInit(): void {
+    combineLatest([
+      this.router.events,
+      this.store.select(organizationFeature.selectSelectedOrganization),
+    ])
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: ([event, selectedOrganization]) => {
+          if (!(event instanceof Scroll)) {
+            return;
+          }
+
+          if (!selectedOrganization) {
+            this.includeCenter = false;
+            return;
+          } else if (
+            event.routerEvent.url ===
+            `/${ShortUrl.Organization}/${selectedOrganization.slug}`
+          ) {
+            this.includeCenter = false;
+            return;
+          }
+
+          this.includeCenter = true;
+        },
+      });
+  }
+
+  /**
+   * Unsubscribe from all infinite observables.
+   */
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 
   /**
    * When the dumb UI emits a `selectedOrganizationChange` event, pass it to the router.
